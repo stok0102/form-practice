c493593d5203b4434e9b3f8f8232889030ea6a32
Chip Carnes and Caleb Stokka <caleb@>
2016-06-01 14:51:23 -0700

    Added javascript

--- survey/js/script.js
+++ survey/js/script.js
@@ -3,5 +3,14 @@
 
 //User inferface logic
 $(document).ready(function() {
-  
+  $("#survey").submit(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
+    var food = $("#food").val();
+    var music = $("input:radio[name=genre]:checked").val();
+    var birthDate = $("#born").val();
+    var color = $("#color").val();
+
+    $("body").css("background", color);
+  });
 });
